69da82c15b75b5ef1646d8db7fd11300da1a14f4
Aman Menda <aman.menda@epitech.eu>
2023-06-14 22:54:48 +0200

    update: runner.h file

--- runner.h
+++ runner.h
@@ -7,10 +7,12 @@
 
 #pragma once
     #include "raylib.h"
+    #include <stdio.h>
 
     #define NUM_FRAMES  3 // Number of frames (rectangles) for the button sprite texture
 
     extern int current_screen;
+    extern float seconds;
 
 enum screen
 {
@@ -37,3 +39,18 @@ typedef struct button
     Rectangle   baseRect;
     Rectangle   bounds;
 } Button_t;
+
+typedef struct menu
+{
+    Font        font;
+    Sound       clickSound;
+    Music       musicStream;
+    Vector2     gameNamePos;
+    Vector2     menuMessagePos;
+    Texture2D   background;
+} Menu_t;
+
+typedef struct game
+{
+    Menu_t mainMenu;
+} Game_t;
